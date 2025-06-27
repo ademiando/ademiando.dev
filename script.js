c5213885d7552d2d118d8ab54cda8fb091a87975
ademiando <ademiandoo@gmail.com>
2025-06-27 15:57:24 +0800

    Update script.js

--- script.js
+++ script.js
@@ -36,8 +36,10 @@ document.addEventListener('DOMContentLoaded', function() {
       if (autoRotate) {
     //  angleX = 15 + 5 * Math.sin(Date.now() / 5000);
     //  angleY += 1;
-        angleX = (angleX + 0.2) % 360;
-        angleY = (angleY + 1) % 360;
+    //  angleX = (angleX + 0.2) % 360;
+    //  angleY = (angleY + 1) % 360;
+        angleX = (angleX + 0.1) % 360;
+        angleY = (angleY + 0.3) % 360;
 
         cube.style.transform = `rotateX(${angleX}deg) rotateY(${angleY}deg)`;
       }
